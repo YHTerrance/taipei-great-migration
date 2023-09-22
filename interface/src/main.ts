c89f126cac992029d44a4af952b5157cb39f0f02
YHTerrance <b08902073@ntu.edu.tw>
2023-09-22 17:27:17 +0800

    Render lines with different weight based on number of passengers, start/end station, start/end time

--- interface/src/main.ts
+++ interface/src/main.ts
@@ -18,6 +18,7 @@ import { Point } from 'ol/geom';
 
 import bezier from '@turf/bezier-spline';
 import { Feature } from 'ol';
+import { containsCoordinate } from 'ol/extent';
 
 // Define the projection
 proj4.defs('EPSG:3826', '+proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +units=m +no_defs');
@@ -28,10 +29,6 @@ const geojsonFormat: GeoJSON = new GeoJSON({
     featureProjection: 'EPSG:3857', // 3857: Flat | 4326: Curved
 });
 
-const strokeStyle = new Stroke({
-    color: "red",
-    width: 2,
-});
 
 // Fetch stations data and draw lines
 const source = new VectorSource();
@@ -39,24 +36,6 @@ const source = new VectorSource();
 const migrations = new VectorLayer({
     source: source
 });
-
-let features: Feature[];
-let stationToIndex: Map;
-let stations: string[];
-
-fetch('/data/metro-station.json')
-    .then(response => response.json())
-    .then(geojsonObject => {
-
-        stations = [];
-        features = geojsonFormat.readFeatures(geojsonObject);
-        stationToIndex = new Map();
-        features.forEach((feature, index) => {
-            const name: string = feature.getProperties()["NAME"];
-            stations.push(name);
-            stationToIndex.set(name, index);
-        });
-    });
 
 const stationsLayer = new VectorLayer({
     source: new VectorSource({
@@ -71,6 +50,56 @@ const metroLinesLayer = new VectorLayer({
         url: '/data/metro-line.json'
     }),
 });
+
+fetch('/data/metro-station.json')
+    .then(response => response.json())
+    .then(geojsonObject => {
+
+        let stations: string[] = [];
+        let features = geojsonFormat.readFeatures(geojsonObject);
+        let stationToIndex = new Map();
+        features.forEach((feature, index) => {
+            const name: string = feature.getProperties()["NAME"];
+            stations.push(name);
+            stationToIndex.set(name, index);
+        });
+
+        generateOptions(stations);
+
+        const renderButton = document.getElementById("render") as HTMLButtonElement;
+
+        renderButton?.addEventListener("click", () => {
+            console.log("render button clicked");
+
+            const fromStation = document.getElementById("from-station") as HTMLSelectElement;
+            const toStation = document.getElementById("to-station") as HTMLSelectElement;
+            const startTime = document.getElementById("start-time") as HTMLSelectElement;
+            const endTime = document.getElementById("end-time") as HTMLSelectElement;
+
+            const body = JSON.stringify({
+                from_station: fromStation.value,
+                to_station: toStation.value,
+                start_time: startTime.value,
+                end_time: endTime.value
+            });
+
+            console.log(body);
+
+            fetch('http://localhost:3000/api/od', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'Application/json'
+                },
+                body: body
+            })
+                .then(response => response.json())
+                .then(data => {
+                    source.clear();
+                    renderLines(features, stationToIndex, fromStation.value, toStation.value, data);
+                })
+                .catch(error => console.error(error));
+        });
+    });
 
 // const base = new TileLayer({
 //     source: new OSM(),
@@ -91,23 +120,81 @@ new Map({
 });
 
 // Helper functions
+function generateOptions(stations: string[]) {
 
-function renderLines() {
-    const fromStation = "石牌";
-    const toStations = ["西湖", "中山", "內湖", "紅樹林"];
+    const fromStation = document.getElementById("from-station") as HTMLSelectElement;
+    const toStation = document.getElementById("to-station") as HTMLSelectElement;
 
-    const fromFeature = features[stationToIndex.get(fromStation + "站")];
-    const toFeatures = toStations.map(station => features[stationToIndex.get(station + "站")]);
+    stations.forEach(station => {
+        const newOption1 = document.createElement("option");
+        newOption1.value = station;
+        newOption1.text = station;
+        const newOption2 = newOption1.cloneNode(true) as HTMLOptionElement;
 
-    toFeatures.forEach(toFeature => {
-        const startCoords = (fromFeature?.getGeometry() as Point).getCoordinates();
-        const endCoords = (toFeature?.getGeometry() as Point).getCoordinates();
+        newOption1.selected = (station == "石牌站");
 
-        addCurve(startCoords, endCoords);
+        fromStation.add(newOption1);
+        toStation.add(newOption2);
     });
+
+    const startTime = document.getElementById("start-time") as HTMLSelectElement;
+    const endTime = document.getElementById("end-time") as HTMLSelectElement;
+
+    for (let time = 0; time < 24; ++time) {
+        const newOption1 = document.createElement("option");
+        newOption1.value = time.toString();
+        newOption1.text = time.toString() + ":00";
+        const newOption2 = newOption1.cloneNode(true) as HTMLOptionElement;
+
+        newOption1.selected = (time == 7);
+        newOption2.selected = (time == 10);
+
+        startTime.add(newOption1);
+        endTime.add(newOption2);
+    }
 }
 
-function addCurve(startCoords: Array<number>, endCoords: Array<number>) {
+function renderLines(features: Feature[], stationToIndex: Map, fromStation: string, toStation: string, data: []) {
+
+    const totalPassengers = data.reduce((acc, station) => acc + station["Total_Passengers"], 0);
+
+    if (fromStation != "null" && toStation != "null") {
+        const fromFeature = features[stationToIndex.get(fromStation)];
+        const toFeature = features[stationToIndex.get(toStation)];
+
+        addCurve(fromFeature, toFeature, 0.5);
+    }
+    else if (fromStation != "null") {
+        const fromFeature = features[stationToIndex.get(fromStation)];
+
+        data.forEach(station => {
+            const toFeature = features[stationToIndex.get(station["出站"] + "站")];
+            const weight = station["Total_Passengers"] / totalPassengers;
+            addCurve(fromFeature, toFeature, weight);
+        });
+    }
+    else if (toStation != "null") {
+        const toFeature = features[stationToIndex.get(toStation)];
+
+        data.forEach(station => {
+            const fromFeature = features[stationToIndex.get(station["進站"] + "站")];
+            const weight = station["Total_Passengers"] / totalPassengers;
+            addCurve(fromFeature, toFeature, weight);
+        });
+    }
+
+}
+
+function addCurve(fromFeature: Feature, toFeature: Feature, weight: number) {
+
+    // console.log(fromFeature, toFeature)
+    if(fromFeature == undefined || toFeature == undefined) {
+        console.log("undefined feature")
+        return;
+    }
+
+    const startCoords = (fromFeature?.getGeometry() as Point).getCoordinates();
+    const endCoords = (toFeature?.getGeometry() as Point).getCoordinates();
 
     const controlCoords = [
         (startCoords[0] + endCoords[0]) / 2,
@@ -131,6 +218,11 @@ function addCurve(startCoords: Array<number>, endCoords: Array<number>) {
 
     const curved = bezier(line);
     const curveFeature = (new GeoJSON()).readFeature(curved);
+
+    const strokeStyle = new Stroke({
+        color: "red",
+        width: 50 * weight,
+    });
 
     curveFeature.setStyle(new Style({
         stroke: strokeStyle,
