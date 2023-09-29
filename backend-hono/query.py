51260ccd1ed5b5a998b7e380572fe7ac6bd1cf13
YHTerrance <b08902073@ntu.edu.tw>
2023-09-29 11:17:12 +0800

    Add max passenger flow query

--- backend-hono/query.py
+++ backend-hono/query.py
@@ -8,11 +8,20 @@ conn = sqlite3.connect('./202307.sqlite')
 cur = conn.cursor()
 
 # SQL query to get the number of people traveling from '石牌' to other stations
+
+'''
 query = """SELECT [出站], SUM([人次]) as Total_Passengers
 FROM [202307]
 WHERE [進站] = '石牌'
 GROUP BY [出站]
 ORDER BY Total_Passengers DESC"""
+'''
+
+query = """SELECT SUM([人次]) as Total_Passengers
+FROM [202307]
+GROUP BY [出站], [進站]
+ORDER BY Total_Passengers DESC
+LIMIT 10"""
 
 # Execute the query
 cur.execute(query)
